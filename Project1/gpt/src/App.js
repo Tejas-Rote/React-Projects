eb0f8c0eec6c646ed6655d0223eca7d6f57b16db
Tejas Rote <tejas292002@gmail.com>
2023-05-16 23:48:07 +0530

    navbar--done

--- Project1/gpt/src/App.js
+++ Project1/gpt/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import './App.css'
 
-import { Footer } from './containers';
-import { Article, Brands } from './components';
+
+import Footer from './containers/footer/Footer.jsx';
+import Header from './containers/header/Header.jsx';
+import Blog from './containers/blog/Blog.jsx';
+import Features from './containers/features/Features.jsx';
+import Possibility from './containers/possibility/Possibility.jsx';
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3.jsx';
+
+
+import { Article, Brand, Cta, Navbar, } from './components';
 const App = () => {
     return (
-        <div>
-            <h1>GPT</h1>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <Cta />
+            <Blog />
+            <Footer />
+
         </div>
+
     )
 }
 
--- Project1/gpt/src/containers/possibilty/Possibilty.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './ossibilty.css'
-const Possibilty = () => {
-  return (
-    <div>Possibilty</div>
-  )
-}
-
-export default Possibilty
